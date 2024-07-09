4f343b2bc82dc2447e1ded74204c9aec39c23e9d
makjac <56642363+makjac@users.noreply.github.com>
2024-07-09 15:10:29 +0200

    feat: create insets.dart

---
+++ lib/core/constans/insets.dart
@@ -0,0 +1,10 @@
+class Insets {
+  static const double none = 0.0;
+  static const double xxs = 4.0;
+  static const double xs = 8.0;
+  static const double s = 16.0;
+  static const double m = 24.0;
+  static const double l = 32.0;
+  static const double xl = 40.0;
+  static const double xxl = 48.0;
+}
