5967196cf50aa95eff7d0df740a6838334676046
makjac <56642363+makjac@users.noreply.github.com>
2024-07-10 15:36:26 +0200

    refactor: change app.dart location

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:quick_note/injection_container.dart';
-import 'package:quick_note/l10n/app_language.dart';
 import 'package:quick_note/l10n/bloc/i10n_bloc.bloc.dart';
 import 'package:quick_note/l10n/l10n.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:quick_note/router/app_router.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -29,58 +28,17 @@ class _App extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<I10nBloc, I10nState>(
       builder: (context, state) {
-        return MaterialApp(
+        return MaterialApp.router(
           title: 'Quick notes',
-          localizationsDelegates: const [
-            AppLocalizations.delegate,
-            GlobalMaterialLocalizations.delegate,
-            GlobalWidgetsLocalizations.delegate,
-            GlobalCupertinoLocalizations.delegate,
-          ],
-          supportedLocales: const [
-            Locale('en'),
-            Locale('pl'),
-          ],
+          routeInformationParser: AppRouter.router.routeInformationParser,
+          routeInformationProvider: AppRouter.router.routeInformationProvider,
+          routerDelegate: AppRouter.router.routerDelegate,
+          localizationsDelegates: AppLocalizations.localizationsDelegates,
+          supportedLocales: AppLocalizations.supportedLocales,
           locale: state.currentLanguage.locale,
-          theme: ThemeData(
-            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-            useMaterial3: true,
-          ),
-          home: const MyHomePage(),
+          debugShowCheckedModeBanner: false,
         );
       },
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(context.l10n.helloWorld),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          ElevatedButton(
-              onPressed: () => BlocProvider.of<I10nBloc>(context)
-                  .add(const ChangeLanguage(language: AppLanguage.english)),
-              child: const Text("English")),
-          ElevatedButton(
-              onPressed: () => BlocProvider.of<I10nBloc>(context)
-                  .add(const ChangeLanguage(language: AppLanguage.polish)),
-              child: const Text("Polish")),
-        ],
-      ),
     );
   }
 }
