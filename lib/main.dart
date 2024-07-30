f9f2ac8997f0204423167d9981a2772e4984779f
makjac <56642363+makjac@users.noreply.github.com>
2024-07-31 00:35:38 +0200

    feat: initialize Firebase in the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:quick_note/app/app.dart';
+import 'package:quick_note/firebase_options.dart';
 import 'package:quick_note/hive/hive.dart' as hive;
 import 'injection_container.dart' as di;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   await hive.init();
   await di.init();
