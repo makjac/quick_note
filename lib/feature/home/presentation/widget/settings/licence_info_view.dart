baf67e5fe0bf8362a9bf89d3192fccac1f8e07a6
makjac <56642363+makjac@users.noreply.github.com>
2024-08-02 07:45:15 +0200

    feat: Add licence information for firebase_crashlytics package

--- lib/feature/home/presentation/widget/settings/licence_info_view.dart
+++ lib/feature/home/presentation/widget/settings/licence_info_view.dart
@@ -393,6 +393,23 @@ class LicenceInfoView extends StatelessWidget {
                   'ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED\n'
                   'WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.',
             ),
+            _PackageLicenceSection(
+              name: "firebase_crashlytics",
+              licence:
+                  'Copyright 2019 The Chromium Authors. All rights reserved.\n\n'
+                  'Redistribution and use in source and binary forms, with or without\n'
+                  'modification, are permitted provided that the following conditions are\n'
+                  'met:\n\n'
+                  '* Redistributions of source code must retain the above copyright\n'
+                  '  notice, this list of conditions and the following disclaimer.\n'
+                  '* Redistributions in binary form must reproduce the above\n'
+                  '  copyright notice, this list of conditions and the following\n'
+                  '  disclaimer in the documentation and/or other materials provided with the distribution.\n'
+                  '* Neither the name of Google Inc. nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.\n\n'
+                  'THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND\n'
+                  'ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED\n'
+                  'WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.',
+            ),
           ],
         ),
       ),
