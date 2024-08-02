2a5d80eb5e7391a771c650f60510e4922bf4712a
makjac <56642363+makjac@users.noreply.github.com>
2024-08-02 07:23:57 +0200

    refactor: Update import statement for AnalyticsManager in analytics_route_observer.dart

--- lib/router/analytics_route_observer.dart
+++ lib/router/analytics_route_observer.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:quick_note/analytics/service/analytics_manager.dart';
+import 'package:quick_note/core/service/analytics/service/analytics_service.dart';
 import 'package:quick_note/injection_container.dart';
 import 'package:quick_note/router/app_routes.dart';
 
 class AnalyticsRouteObserver extends NavigatorObserver {
   @override
   void didPush(Route<dynamic> route, Route<dynamic>? previousRoute) async {
-    final analytics = locator<AnalyticsManager>();
+    final analytics = locator<AnalyticsService>();
 
     if (route.settings.name != null) {
       analytics.logScreenView(route.settings.name ?? AppRoutes.unknown.name);
