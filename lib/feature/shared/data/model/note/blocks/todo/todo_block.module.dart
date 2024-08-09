6ca7489966a48be45c81e75717297db5da35cf16
makjac <56642363+makjac@users.noreply.github.com>
2024-08-10 01:38:02 +0200

    chore: relocate files to new directory

--- lib/feature/shared/data/model/note/blocks/todo/todo_block.module.dart
+++ lib/feature/shared/data/model/note/blocks/todo/todo_block.module.dart
@@ -1,9 +1,9 @@
 // ignore_for_file: must_be_immutable, overridden_fields
 
 import 'package:hive/hive.dart';
-import 'package:quick_note/feature/shared/data/model/note/blocks/check_list_item.module.dart';
+import 'package:quick_note/feature/shared/data/model/note/blocks/todo/check_list_item.module.dart';
 import 'package:quick_note/feature/shared/data/model/note/note_block.module.dart';
-import 'package:quick_note/feature/shared/domain/entity/note/blocks/todo_block.dart';
+import 'package:quick_note/feature/shared/domain/entity/note/blocks/todo/todo_block.dart';
 import 'package:quick_note/feature/shared/domain/entity/note/note_block_type.dart';
 
 part 'todo_block.module.g.dart';
--- lib/feature/shared/domain/entity/note/blocks/todo/todo_block.dart
+++ lib/feature/shared/domain/entity/note/blocks/todo/todo_block.dart
@@ -1,5 +1,5 @@
 import 'package:hive/hive.dart';
-import 'package:quick_note/feature/shared/domain/entity/note/blocks/check_list_item.dart';
+import 'package:quick_note/feature/shared/domain/entity/note/blocks/todo/check_list_item.dart';
 import 'package:quick_note/feature/shared/domain/entity/note/note_block.dart';
 import 'package:quick_note/feature/shared/domain/entity/note/note_block_type.dart';
 
