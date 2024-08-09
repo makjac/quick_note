d1d325139cc3724d73b87860236e9fdeed578a48
makjac <56642363+makjac@users.noreply.github.com>
2024-08-10 01:50:20 +0200

    feat: Implement fetchAllFaviconUrls method in BookmarksRemoteDatasourceImpl

--- lib/feature/notebook/data/datasource/bookmarks_remote_datasource.dart
+++ lib/feature/notebook/data/datasource/bookmarks_remote_datasource.dart
@@ -8,3 +8,56 @@ abstract class BookmarksRemoteDatasource {
   Future<bool> isValidFaviconUrl(String url);
   Future<List<FaviconDataModel>> fetchFaviconsByDomain(String domain);
 }
+class BookmarksRemoteDatasourceImpl implements BookmarksRemoteDatasource {
+  BookmarksRemoteDatasourceImpl({http.Client? client})
+      : client = client ?? http.Client();
+
+  final http.Client client;
+
+  @override
+  Future<List<FaviconDataModel>> fetchAllFaviconUrls(String url) async {
+    var favicons = <FaviconDataModel>[];
+    var iconUrls = <String>[];
+
+    var uri = Uri.parse(url);
+    var document = html_parser.parse((await client.get(uri)).body);
+
+    for (var rel in ['icon', 'shortcut icon', 'apple-touch-icon']) {
+      for (var iconTag in document.querySelectorAll("link[rel='$rel']")) {
+        if (iconTag.attributes['href'] != null) {
+          var iconUrl = _sanitizeUrl(iconTag.attributes['href']!.trim(), uri);
+          if (await _verifyImage(iconUrl)) {
+            iconUrls.add(iconUrl);
+          }
+        }
+      }
+    }
+
+    var iconUrl = '${uri.scheme}://${uri.host}/favicon.ico';
+    if (await _verifyImage(iconUrl)) {
+      iconUrls.add(iconUrl);
+    }
+
+    iconUrls = iconUrls.toSet().toList();
+
+    for (var iconUrl in iconUrls) {
+      if (iconUrl.endsWith('.svg')) {
+        favicons.add(FaviconDataModel(url: iconUrl));
+        continue;
+      }
+
+      if (iconUrl.endsWith('.ico')) {
+        favicons.add(FaviconDataModel(url: iconUrl, sizes: '16x16'));
+        continue;
+      }
+
+      var image = decodeImage((await client.get(Uri.parse(iconUrl))).bodyBytes);
+      if (image != null) {
+        favicons.add(FaviconDataModel(
+            url: iconUrl, sizes: '${image.width}x${image.height}'));
+      }
+    }
+
+    return favicons;
+  }
+  }
