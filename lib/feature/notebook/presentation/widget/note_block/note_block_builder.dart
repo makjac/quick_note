a640c9b0f90c42b9f2132b27b47d7fbcad726d75
makjac <56642363+makjac@users.noreply.github.com>
2024-08-10 02:27:48 +0200

    feat: Add support for bookmarks block widget in NoteBlockBuilder

--- lib/feature/notebook/presentation/widget/note_block/note_block_builder.dart
+++ lib/feature/notebook/presentation/widget/note_block/note_block_builder.dart
@@ -9,11 +9,15 @@ import 'package:quick_note/core/constans/insets.dart';
 import 'package:quick_note/core/utils/platform_helper.dart';
 import 'package:quick_note/core/utils/text_formaters/only_positive_integer_formatter.dart';
 import 'package:quick_note/feature/notebook/presentation/bloc/notebook_bloc.dart';
+import 'package:quick_note/feature/notebook/presentation/cubit/bookmarks_block_cubit/bookmarks_block_cubit.dart';
 import 'package:quick_note/feature/notebook/presentation/cubit/text_block_cubit/text_block_cubit.dart';
 import 'package:quick_note/feature/notebook/presentation/cubit/todo_block_cubit/todo_block_cubit.dart';
-import 'package:quick_note/feature/shared/domain/entity/note/blocks/check_list_item.dart';
-import 'package:quick_note/feature/shared/domain/entity/note/blocks/text_block.dart';
-import 'package:quick_note/feature/shared/domain/entity/note/blocks/todo_block.dart';
+import 'package:quick_note/feature/notebook/presentation/widget/note_block/blocks/bookmarks/bookmarks_block_widget.dart';
+import 'package:quick_note/feature/shared/domain/entity/note/blocks/bookmarks/bookmark_view_mode.dart';
+import 'package:quick_note/feature/shared/domain/entity/note/blocks/bookmarks/bookmarks_block.dart';
+import 'package:quick_note/feature/shared/domain/entity/note/blocks/todo/check_list_item.dart';
+import 'package:quick_note/feature/shared/domain/entity/note/blocks/text/text_block.dart';
+import 'package:quick_note/feature/shared/domain/entity/note/blocks/todo/todo_block.dart';
 import 'package:quick_note/feature/shared/domain/entity/note/note_block.dart';
 import 'package:quick_note/feature/shared/domain/entity/note/note_block_type.dart';
 import 'package:quick_note/l10n/l10n.dart';
@@ -29,6 +33,7 @@ part 'block_settings/shared/note_block_modal_bottom_sheet.dart';
 part 'block_settings/note_block_settings.dart';
 part 'block_settings/text/text_block_settings_dialog.dart';
 part 'block_settings/todo/todo_block_settings_dialog.dart';
+part 'block_settings/bookmarks/bookmarks_block_settings_dialog.dart';
 
 part 'blocks/shared/note_block_title.dart';
 
