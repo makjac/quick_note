1b9b8c7bb6769ef2a1b15924d7649891ea828e79
makjac <56642363+makjac@users.noreply.github.com>
2024-08-10 02:29:01 +0200

    feat: Add BookmarksBlockCubit to handle bookmarks block state and fetching favicon URLs

---
+++ lib/feature/notebook/presentation/cubit/bookmarks_block_cubit/bookmarks_block_cubit.dart
@@ -0,0 +1,20 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:quick_note/core/utils/url_helper.dart';
+import 'package:quick_note/feature/notebook/domain/usecase/fetch_best_favicon_url_usecase.dart';
+import 'package:quick_note/feature/shared/domain/entity/note/blocks/bookmarks/bookmark_item.dart';
+import 'package:quick_note/feature/shared/domain/entity/note/blocks/bookmarks/bookmark_view_mode.dart';
+import 'package:quick_note/feature/shared/domain/entity/note/blocks/bookmarks/bookmarks_block.dart';
+
+part 'bookmarks_block_state.dart';
+
+class BookmarksBlockCubit extends Cubit<BookmarksBlockState> {
+  BookmarksBlockCubit({
+    BookmarksBlock? block,
+    required this.fetchBestFaviconUrlUsecase,
+  }) : super(BookmarksBlockState(block: block));
+
+  final FetchBestFaviconUrlUsecase fetchBestFaviconUrlUsecase;
+}
