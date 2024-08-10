eb453fe2d63701bf87e39ba6ef084f2d3fc5eb03
makjac <56642363+makjac@users.noreply.github.com>
2024-08-10 02:03:31 +0200

    feat: Add fetchAllFaviconUrls method to BookmarkRepositoryImpl

--- lib/feature/notebook/data/repository/bookmarks_repository_impl.dart
+++ lib/feature/notebook/data/repository/bookmarks_repository_impl.dart
@@ -1,6 +1,8 @@
 import 'package:dartz/dartz.dart';
+import 'package:quick_note/core/error/failure/Parsing_failure.dart';
 import 'package:quick_note/core/error/failure/failure.dart';
 import 'package:quick_note/core/error/failure/network_failure.dart';
+import 'package:quick_note/core/error/failure/validation_failure.dart';
 import 'package:quick_note/feature/notebook/data/datasource/bookmarks_remote_datasource.dart';
 import 'package:quick_note/feature/notebook/domain/entity/favicon_data.dart';
 import 'package:quick_note/feature/notebook/domain/repository/bookmarks_repository.dart';
@@ -14,6 +16,17 @@ class BookmarkRepositoryImpl implements BookmarkRepository {
   Future<Either<Failure, String?>> fetchBestFaviconUrl(String url) async {
     try {
       final result = await datasource.fetchBestFaviconUrl(url);
+      return Right(result);
+    } catch (e) {
+      return const Left(NetworkFailure());
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<FaviconData>>> fetchAllFaviconUrls(
+      String url) async {
+    try {
+      final result = await datasource.fetchAllFaviconUrls(url);
       return Right(result);
     } catch (e) {
       return const Left(NetworkFailure());
