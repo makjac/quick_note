8d51116d68a8a5238409b8717f34c464d58c6ff5
makjac <56642363+makjac@users.noreply.github.com>
2024-08-11 05:20:34 +0200

    test: implements getNotes successfully retrieves notes from Hive

---
+++ test/feature/home/data/datasource/app_local_data_source_test.dart
@@ -0,0 +1,49 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:hive/hive.dart';
+import 'package:quick_note/core/error/exception/cache_exception.dart';
+import 'package:quick_note/feature/home/data/datasource/app_local_data_source.dart';
+import 'package:quick_note/feature/shared/data/model/note/note.module.dart';
+import 'package:quick_note/hive/hive_keys.dart';
+
+class MockHiveInterface extends Mock implements HiveInterface {}
+
+class MockBox extends Mock implements Box<NoteModel> {}
+
+void main() {
+  late MockHiveInterface mockHive;
+  late MockBox mockBox;
+  late AppLocalDataSourceImpl localDataSource;
+
+  setUp(() {
+    mockHive = MockHiveInterface();
+    mockBox = MockBox();
+    localDataSource = AppLocalDataSourceImpl(hive: mockHive);
+  });
+
+  group('AppLocalDataSourceImpl', () {
+    test('should return a list of notes when getNotes is called successfully',
+        () async {
+      // Arrange
+      final notes = [
+        NoteModel(
+          id: 1,
+          created: DateTime.now(),
+          modified: DateTime.now(),
+          title: 'Test Note',
+          content: const [],
+        )
+      ];
+      when(() => mockHive.openBox<NoteModel>(HiveBoxes.note.name))
+          .thenAnswer((_) async => mockBox);
+      when(() => mockBox.values).thenReturn(notes);
+
+      // Act
+      final result = await localDataSource.getNotes();
+
+      // Assert
+      expect(result, notes);
+      verify(() => mockHive.openBox<NoteModel>(HiveBoxes.note.name)).called(1);
+    });
+  });
+}
