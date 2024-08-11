0436ee69d49ed3eecdb1c96bc4a42e6720ec2e15
makjac <56642363+makjac@users.noreply.github.com>
2024-08-11 05:36:56 +0200

    test: return Note when repository returns a note

---
+++ test/feature/notebook/domain/usecase/get_note_by_key_usecase_test.dart
@@ -0,0 +1,43 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:quick_note/core/error/failure/cache_failure.dart';
+import 'package:quick_note/core/error/failure/cache_value_failure.dart';
+import 'package:quick_note/core/error/failure/unknown_failure.dart';
+import 'package:quick_note/feature/notebook/domain/repository/notebook_repository.dart';
+import 'package:quick_note/feature/shared/domain/entity/note/note.dart';
+import 'package:quick_note/feature/notebook/domain/usecase/get_note_by_key_usecase.dart';
+
+class MockNotebookRepository extends Mock implements NotebookRepository {}
+
+void main() {
+  late GetNoteByKeyUsecase usecase;
+  late MockNotebookRepository mockRepository;
+
+  setUp(() {
+    mockRepository = MockNotebookRepository();
+    usecase = GetNoteByKeyUsecase(repository: mockRepository);
+  });
+
+  const tKey = 1;
+  final tNote = Note(
+    id: tKey,
+    created: DateTime.now(),
+    modified: DateTime.now(),
+  );
+
+  group('GetNoteByKeyUsecase', () {
+    test('should return Note when repository returns a note', () async {
+      // Arrange
+      when(() => mockRepository.getNoteByKey(tKey))
+          .thenAnswer((_) async => Right(tNote));
+
+      // Act
+      final result = await usecase(tKey);
+
+      // Assert
+      expect(result, equals(Right(tNote)));
+      verify(() => mockRepository.getNoteByKey(tKey)).called(1);
+    });
+  });
+}
