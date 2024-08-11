a28b06bf8df04554bca9a5e5ab7ba7e8f2992bb5
makjac <56642363+makjac@users.noreply.github.com>
2024-08-11 03:56:20 +0200

    test: add test for correct values in SharedKeys enum

--- test/core/constans/shared_keys_test.dart
+++ test/core/constans/shared_keys_test.dart
@@ -6,5 +6,11 @@ void main() {
     test('should have correct number of values', () {
       expect(SharedKeys.values.length, 3);
     });
+
+    test('should have correct values', () {
+      expect(SharedKeys.values[0], SharedKeys.languagePref);
+      expect(SharedKeys.values[1], SharedKeys.themePref);
+      expect(SharedKeys.values[2], SharedKeys.colapseSideMenuPerf);
+    });
   });
 }
