d1ad11fc05a76ad5a63ff0c29be940b509344cb1
makjac <56642363+makjac@users.noreply.github.com>
2024-08-11 05:24:40 +0200

    test: add test for handling CacheException when updating a note in AppLocalDataSource

---
+++ test/feature/home/data/repository/app_repository_impl_test.dart
@@ -0,0 +1,45 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:dartz/dartz.dart';
+import 'package:quick_note/core/error/failure/cache_failure.dart';
+import 'package:quick_note/feature/home/data/datasource/app_local_data_source.dart';
+import 'package:quick_note/feature/home/data/repository/app_repository_impl.dart';
+import 'package:quick_note/feature/home/domain/usecase/update_multiple_notes_usecase.dart';
+import 'package:quick_note/feature/shared/data/model/note/note.module.dart';
+import 'package:quick_note/feature/shared/domain/entity/note/note.dart';
+
+class MockAppLocalDataSource extends Mock implements AppLocalDataSource {}
+
+void main() {
+  late MockAppLocalDataSource mockLocalDataSource;
+  late AppRepositoryImpl repository;
+
+  setUp(() {
+    mockLocalDataSource = MockAppLocalDataSource();
+    repository = AppRepositoryImpl(localDataSource: mockLocalDataSource);
+  });
+
+  group('AppRepositoryImpl', () {
+    final note = Note(
+      id: 1,
+      title: 'Test Note',
+      content: const [],
+      created: DateTime.now(),
+      modified: DateTime.now(),
+    );
+    final noteModel = NoteModel.fromEntity(note);
+    const noteUpdates = NoteUpdates(title: 'Updated Title', content: null);
+
+    test('should return CacheFailure when getAllNotes throws an exception',
+        () async {
+      // Arrange
+      when(() => mockLocalDataSource.getNotes()).thenThrow(Exception());
+
+      // Act
+      final result = await repository.getAllNotes();
+
+      // Assert
+      expect(result, equals(const Left(CacheFailure())));
+    });
+  });
+}
