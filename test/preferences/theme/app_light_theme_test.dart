b4ece95e66afd70a214ec1c85a6e634e3af93888
makjac <56642363+makjac@users.noreply.github.com>
2024-08-11 04:41:56 +0200

    test: verify lightTheme uses Material3

---
+++ test/preferences/theme/app_light_theme_test.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:quick_note/core/constans/insets.dart';
+import 'package:quick_note/preferences/theme/app_light_theme.dart';
+
+void main() {
+  group('lightTheme', () {
+    test('should use Material3', () {
+      expect(lightTheme.useMaterial3, isTrue);
+    });
+  });
