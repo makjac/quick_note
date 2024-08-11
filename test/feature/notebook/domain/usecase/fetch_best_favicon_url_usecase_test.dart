498d9f3e9af3b0e8984b0681d0e161ee6be53954
makjac <56642363+makjac@users.noreply.github.com>
2024-08-11 05:36:19 +0200

    test: return best favicon URL when repository call is successful

---
+++ test/feature/notebook/domain/usecase/fetch_best_favicon_url_usecase_test.dart
@@ -0,0 +1,61 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:quick_note/core/error/failure/network_failure.dart';
+import 'package:quick_note/feature/notebook/domain/repository/bookmarks_repository.dart';
+import 'package:quick_note/feature/notebook/domain/usecase/fetch_best_favicon_url_usecase.dart';
+
+// Tworzymy klasę mockującą interfejs BookmarkRepository
+class MockBookmarkRepository extends Mock implements BookmarkRepository {}
+
+void main() {
+  late FetchBestFaviconUrlUsecase usecase;
+  late MockBookmarkRepository mockRepository;
+
+  setUp(() {
+    mockRepository = MockBookmarkRepository();
+    usecase = FetchBestFaviconUrlUsecase(repository: mockRepository);
+  });
+
+  const testUrl = 'https://example.com';
+  const expectedFaviconUrl = 'https://example.com/favicon.ico';
+
+  // Rejestracja parametrów, które będą używane w mockowaniu
+  setUpAll(() {
+    registerFallbackValue(testUrl);
+  });
+
+  group('FetchBestFaviconUrlUsecase', () {
+    test(
+        'should return the best Favicon URL when the repository call is successful',
+        () async {
+      // arrange
+      when(() => mockRepository.fetchBestFaviconUrl(any()))
+          .thenAnswer((_) async => const Right(expectedFaviconUrl));
+
+      // act
+      final result = await usecase.call(testUrl);
+
+      // assert
+      expect(result, const Right(expectedFaviconUrl));
+      verify(() => mockRepository.fetchBestFaviconUrl(testUrl)).called(1);
+      verifyNoMoreInteractions(mockRepository);
+    });
+
+    test('should return a Failure when the repository call is unsuccessful',
+        () async {
+      // arrange
+      const failure = NetworkFailure();
+      when(() => mockRepository.fetchBestFaviconUrl(any()))
+          .thenAnswer((_) async => const Left(failure));
+
+      // act
+      final result = await usecase.call(testUrl);
+
+      // assert
+      expect(result, const Left(failure));
+      verify(() => mockRepository.fetchBestFaviconUrl(testUrl)).called(1);
+      verifyNoMoreInteractions(mockRepository);
+    });
+  });
+}
