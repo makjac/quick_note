b897ba069920e298fb3badb5942d8be2815d2b66
makjac <56642363+makjac@users.noreply.github.com>
2024-08-11 04:16:50 +0200

    test: add test for AnalyticsService.logScreenView method

---
+++ test/core/service/analytics/service/analytics_service_test.dart
@@ -0,0 +1,31 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:mockito/annotations.dart';
+import 'package:quick_note/core/service/analytics/enum/note_action.dart';
+import 'package:quick_note/core/service/analytics/service/analytics_service.dart';
+import 'package:quick_note/feature/shared/domain/entity/note/note_block_type.dart';
+
+import 'analytics_service_test.mocks.dart';
+
+@GenerateMocks([FirebaseAnalytics])
+void main() {
+  group('AnalyticsService', () {
+    late MockFirebaseAnalytics mockFirebaseAnalytics;
+    late AnalyticsService analyticsService;
+
+    setUp(() {
+      mockFirebaseAnalytics = MockFirebaseAnalytics();
+      analyticsService = AnalyticsService(analytics: mockFirebaseAnalytics);
+    });
+
+    test(
+        'logScreenView calls FirebaseAnalytics.logScreenView with correct parameters',
+        () {
+      analyticsService.logScreenView('home_screen');
+
+      verify(mockFirebaseAnalytics.logScreenView(screenName: 'home_screen'))
+          .called(1);
+    });
+  });
+}
