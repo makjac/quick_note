bbb607a0e7f9fdcf2e917f8155cdc72aa8927737
makjac <56642363+makjac@users.noreply.github.com>
2024-08-11 04:27:27 +0200

    test: add unit test for UrlHelper.isValidUrl

---
+++ test/core/utils/url_helper_test.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:quick_note/core/utils/url_helper.dart';
+
+void main() {
+  group('UrlHelper', () {
+    group('isValidUrl', () {
+      test('returns true for valid URLs', () {
+        expect(UrlHelper.isValidUrl('http://www.example.com'), isTrue);
+        expect(UrlHelper.isValidUrl('https://example.com'), isTrue);
+        expect(UrlHelper.isValidUrl('http://sub.example.com'), isTrue);
+        expect(UrlHelper.isValidUrl('http://192.168.1.1'), isTrue);
+        expect(UrlHelper.isValidUrl('http://example.co.uk'), isTrue);
+      });
+
+      test('returns false for invalid URLs', () {
+        expect(UrlHelper.isValidUrl('htp://example.com'), isFalse);
+        expect(UrlHelper.isValidUrl('http://-example.com'), isFalse);
+        expect(UrlHelper.isValidUrl('http://example..com'), isFalse);
+        expect(UrlHelper.isValidUrl('http://.com'), isFalse);
+        expect(UrlHelper.isValidUrl('example'), isFalse);
+      });
+    });
+    });
+  });
+}
