b0ef00cc4def1f7951bb46b2ce77d2d3265d0a0d
makjac <56642363+makjac@users.noreply.github.com>
2024-08-11 04:01:00 +0200

    test: add test to verify CacheValueFailure is an instance of Failure

---
+++ test/core/error/failure/cache_value_failure_test.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:quick_note/core/error/failure/failure.dart';
+import 'package:quick_note/core/error/failure/cache_value_failure.dart';
+
+void main() {
+  group('CacheValueFailure', () {
+    test('should be an instance of Failure', () {
+      const failure = CacheValueFailure();
+      expect(failure, isA<Failure>());
+    });
+  });
+}
