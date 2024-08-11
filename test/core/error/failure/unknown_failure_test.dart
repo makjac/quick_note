2c2fcb26f5c2eade52df3438bb591cc1afee6336
makjac <56642363+makjac@users.noreply.github.com>
2024-08-11 04:07:29 +0200

    test: add test to verify UnknownFailure is an instance of Failure

---
+++ test/core/error/failure/unknown_failure_test.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:quick_note/core/error/failure/failure.dart';
+import 'package:quick_note/core/error/failure/unknown_failure.dart';
+
+void main() {
+  group('UnknownFailure', () {
+    test('should be an instance of Failure', () {
+      const failure = UnknownFailure();
+      expect(failure, isA<Failure>());
+    });
+  });
+}
