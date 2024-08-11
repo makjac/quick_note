b7374eb813085a3c373e94399b1fb680125b3a2b
makjac <56642363+makjac@users.noreply.github.com>
2024-08-11 04:08:57 +0200

    test: add test to verify ValidationFailure is an instance of Failure

---
+++ test/core/error/failure/validation_failure_test.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:quick_note/core/error/failure/failure.dart';
+import 'package:quick_note/core/error/failure/validation_failure.dart';
+
+void main() {
+  group('ValidationFailure', () {
+    test('should be an instance of Failure', () {
+      const failure = ValidationFailure();
+      expect(failure, isA<Failure>());
+    });
+  });
+}
