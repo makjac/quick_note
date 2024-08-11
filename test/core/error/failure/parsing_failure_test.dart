ce6e423ab041df1aefbdaeffb7139f074e8994b3
makjac <56642363+makjac@users.noreply.github.com>
2024-08-11 04:06:01 +0200

    test: add test to verify ParsingFailure is an instance of Failure

---
+++ test/core/error/failure/parsing_failure_test.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:quick_note/core/error/failure/failure.dart';
+import 'package:quick_note/core/error/failure/parsing_failure.dart';
+
+void main() {
+  group('ParsingFailure', () {
+    test('should be an instance of Failure', () {
+      const failure = ParsingFailure();
+      expect(failure, isA<Failure>());
+    });
+  });
+}
