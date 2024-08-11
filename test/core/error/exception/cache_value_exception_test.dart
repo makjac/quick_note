d6713e506460ab7ebba94c42bd584f6d626d616d
makjac <56642363+makjac@users.noreply.github.com>
2024-08-11 03:58:10 +0200

    test: add test to verify CacheValueException is an instance of Exception

---
+++ test/core/error/exception/cache_value_exception_test.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:quick_note/core/error/exception/cache_value_exception.dart';
+
+void main() {
+  group('CacheValueException', () {
+    test('should be an instance of Exception', () {
+      final exception = CacheValueException();
+      expect(exception, isA<Exception>());
+    });
+  });
+}
