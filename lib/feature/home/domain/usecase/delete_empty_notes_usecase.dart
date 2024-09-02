fd0127c7a744b197bea2404d84a7bae643195b36
makjac <56642363+makjac@users.noreply.github.com>
2024-09-03 00:44:06 +0200

    style: reposition constructor of DeleteEmptyNotesUsecase to top of class

--- lib/feature/home/domain/usecase/delete_empty_notes_usecase.dart
+++ lib/feature/home/domain/usecase/delete_empty_notes_usecase.dart
@@ -4,9 +4,9 @@ import 'package:quick_note/core/usecase/usecase.dart';
 import 'package:quick_note/feature/home/domain/repository/app_repository.dart';
 
 class DeleteEmptyNotesUsecase extends UseCase<void, NoParams> {
-  final AppRepository repository;
-
   DeleteEmptyNotesUsecase({required this.repository});
+
+  final AppRepository repository;
 
   @override
   Future<Either<Failure, void>> call(NoParams params) async {
