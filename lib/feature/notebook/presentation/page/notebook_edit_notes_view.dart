a98fd31741471df8159ab1a0fa69bf2561fe96f5
makjac <56642363+makjac@users.noreply.github.com>
2024-09-09 03:39:22 +0200

    chore: import statements in notebook_edit_notes_view.dart

--- lib/feature/notebook/presentation/page/notebook_edit_notes_view.dart
+++ lib/feature/notebook/presentation/page/notebook_edit_notes_view.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:quick_note/core/constans/app_constans.dart';
 import 'package:quick_note/core/constans/insets.dart';
-import 'package:quick_note/feature/notebook/presentation/widget/Notebook_desktop__icons_menu/notebook_desktop_icons_menu.dart';
+import 'package:quick_note/feature/notebook/presentation/widget/Notebook_desktop_icons_menu/notebook_desktop_icons_menu.dart';
 import 'package:quick_note/feature/notebook/presentation/widget/add_note_block_button/add_note_block_button.dart';
 import 'package:quick_note/feature/notebook/presentation/widget/note_block/note_block_builder.dart';
-import 'package:quick_note/feature/notebook/presentation/widget/notebook_popup_menu.dart/notebook_popup_menu.dart';
+import 'package:quick_note/feature/notebook/presentation/widget/notebook_popup_menu/notebook_popup_menu.dart';
 import 'package:quick_note/feature/notebook/presentation/widget/notebook_title_text_field.dart';
 import 'package:quick_note/feature/shared/domain/entity/note/note.dart';
 import 'package:quick_note/feature/shared/domain/entity/note/note_colors.dart';
@@ -24,22 +24,21 @@ class _NotebookEditNotesViewState extends State<NotebookEditNotesView> {
   late FocusNode _focusNode;
 
   bool get _hasTitle => widget.note?.title.isNotEmpty ?? false;
-
   bool get _setFocus => !_hasTitle && widget.note != null;
 
   @override
   void initState() {
-    _focusNode = FocusNode();
-
     super.initState();
+    _focusNode = FocusNode();
   }
 
   @override
   void didUpdateWidget(covariant NotebookEditNotesView oldWidget) {
-    if (oldWidget.note == null) {
-      _setFocus ? (_focusNode..requestFocus()) : (_focusNode..unfocus());
-    }
     super.didUpdateWidget(oldWidget);
+
+    if (oldWidget.note == null) {
+      _setFocus ? _focusNode.requestFocus() : _focusNode.unfocus();
+    }
   }
 
   @override
