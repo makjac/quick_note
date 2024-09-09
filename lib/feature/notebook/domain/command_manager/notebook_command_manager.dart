83010864e2b6058b7ca1a5911faa3c501a665a26
makjac <56642363+makjac@users.noreply.github.com>
2024-09-09 02:33:11 +0200

    feat: add NotebookCommandManager with undo/redo functionality

---
+++ lib/feature/notebook/domain/command_manager/notebook_command_manager.dart
@@ -0,0 +1,15 @@
+import 'package:quick_note/feature/home/domain/usecase/update_multiple_notes_usecase.dart';
+import 'package:quick_note/feature/notebook/domain/command/notebook_command.dart';
+
+abstract class NotebookCommandManager {
+  NotebookCommandType? get getCurrentUndoType;
+  NotebookCommandType? get getCurrentRedoType;
+  NoteUpdates execute(NotebookCommand command);
+  NoteUpdates? undo();
+  NoteUpdates? redo();
+  void pushToUndoStack(NotebookCommand command);
+  void pushToRedoStack(NotebookCommand command);
+  NotebookCommand? popUndoStack();
+  NotebookCommand? popRedoStack();
+  void clear();
+}
