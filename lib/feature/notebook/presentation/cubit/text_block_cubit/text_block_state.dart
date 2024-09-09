671e490705d614f4698cb7f6ec95be8d50f354b2
makjac <56642363+makjac@users.noreply.github.com>
2024-09-09 04:08:04 +0200

    refactor: `TextBlockState` include a `NotebookCommand`

--- lib/feature/notebook/presentation/cubit/text_block_cubit/text_block_state.dart
+++ lib/feature/notebook/presentation/cubit/text_block_cubit/text_block_state.dart
@@ -1,13 +1,20 @@
 part of 'text_block_cubit.dart';
 
 class TextBlockState extends Equatable {
-  const TextBlockState({TextBlock? block})
+  const TextBlockState({TextBlock? block, this.command})
       : block = block ?? const TextBlock(id: -1);
 
   final TextBlock block;
+  final NotebookCommand? command;
 
-  TextBlockState copyWith({TextBlock? block}) {
-    return TextBlockState(block: block ?? this.block);
+  TextBlockState copyWith({
+    TextBlock? block,
+    NotebookCommand? command,
+  }) {
+    return TextBlockState(
+      block: block ?? this.block,
+      command: command,
+    );
   }
 
   @override
