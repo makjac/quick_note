391dec6b95f5a485045d61b89bec984f8b438bcf
makjac <56642363+makjac@users.noreply.github.com>
2024-09-10 02:52:25 +0200

    feat: add NotebookColorButton widget for color selection

---
+++ lib/feature/notebook/presentation/widget/notebook_leyout/notebook_action_buttons/notebook_color_button.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:quick_note/core/utils/note_helper.dart';
+import 'package:quick_note/feature/notebook/presentation/bloc/notebook_bloc.dart';
+import 'package:quick_note/l10n/l10n.dart';
+import 'package:quick_note/preferences/theme/app_custom_colors.dart';
+
+class NotebookColorButton extends StatelessWidget {
+  const NotebookColorButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: () async {
+        final color = await NoteHelper.showNoteColorPickerDialog(context);
+        if (color != null) {
+          if (!context.mounted) return;
+          BlocProvider.of<NotebookBloc>(context).add(
+            NotebookChangeColor(color: color),
+          );
+        }
+      },
+      icon: Tooltip(
+        message: context.l10n.note_settings_color,
+        child: Icon(
+          Icons.color_lens_outlined,
+          color: Theme.of(context).editHeaderForegroundColor?.withAlpha(200),
+        ),
+      ),
+    );
+  }
+}
