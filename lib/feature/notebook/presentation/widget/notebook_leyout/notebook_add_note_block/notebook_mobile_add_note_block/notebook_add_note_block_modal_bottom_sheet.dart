c55c4cdff123eb105ffbb76124e73cd242edc932
makjac <56642363+makjac@users.noreply.github.com>
2024-09-17 04:28:47 +0200

    refactor: delegate BottomSheet display to ShowAddNoteBlockBottomSheet

--- lib/feature/notebook/presentation/widget/notebook_leyout/notebook_add_note_block/notebook_mobile_add_note_block/notebook_add_note_block_modal_bottom_sheet.dart
+++ lib/feature/notebook/presentation/widget/notebook_leyout/notebook_add_note_block/notebook_mobile_add_note_block/notebook_add_note_block_modal_bottom_sheet.dart
@@ -1,12 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:quick_note/core/constans/insets.dart';
-import 'package:quick_note/core/service/analytics/service/analytics_service.dart';
-import 'package:quick_note/core/utils/platform_helper.dart';
-import 'package:quick_note/feature/notebook/presentation/bloc/notebook_bloc.dart';
-import 'package:quick_note/feature/shared/domain/entity/note/note_block_type.dart';
-import 'package:quick_note/injection_container.dart';
-import 'package:quick_note/l10n/l10n.dart';
+import 'package:quick_note/feature/notebook/presentation/widget/notebook_leyout/notebook_add_note_block/notebook_mobile_add_note_block/show_add_note_block_bottom_sheet.dart';
 
 class NotebookAddNoteBlockModalBottomSheet extends StatelessWidget {
   const NotebookAddNoteBlockModalBottomSheet({super.key});
@@ -14,74 +7,8 @@ class NotebookAddNoteBlockModalBottomSheet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return IconButton(
-      onPressed: () => _showAddNoteBlockModalBottomSheet(context),
+      onPressed: () => ShowAddNoteBlockBottomSheet.show(context),
       icon: const Icon(Icons.add_box_outlined),
-    );
-  }
-
-  void logCreateNoteEvent(NoteBlockType blockType) {
-    if (PlatformHelper.isMobile()) {
-      locator<AnalyticsService>().logAddBlockEvent(blockType);
-    }
-  }
-
-  void _showAddNoteBlockModalBottomSheet(BuildContext context) {
-    if (!context.mounted) return;
-
-    final notebookBloc = BlocProvider.of<NotebookBloc>(context);
-
-    showModalBottomSheet(
-      context: context,
-      builder: (context) {
-        return Padding(
-          padding: const EdgeInsets.all(Insets.s),
-          child: Wrap(
-            children: [
-              Padding(
-                padding:
-                    const EdgeInsets.only(left: Insets.s, bottom: Insets.xs),
-                child: Text(
-                  "${context.l10n.add_note_block}:",
-                  style: Theme.of(context).textTheme.titleMedium,
-                ),
-              ),
-              ListTile(
-                leading: const Icon(Icons.text_fields),
-                title: Text(context.l10n.block_type_text),
-                onTap: () {
-                  notebookBloc.add(
-                    const NotebookAddNoteBlock(type: NoteBlockType.text),
-                  );
-                  logCreateNoteEvent(NoteBlockType.text);
-                  Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                leading: const Icon(Icons.check_box_outlined),
-                title: Text(context.l10n.block_type_todo),
-                onTap: () {
-                  notebookBloc.add(
-                    const NotebookAddNoteBlock(type: NoteBlockType.todo),
-                  );
-                  logCreateNoteEvent(NoteBlockType.todo);
-                  Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                leading: const Icon(Icons.bookmark_border),
-                title: Text(context.l10n.block_bookmark_todo),
-                onTap: () {
-                  notebookBloc.add(
-                    const NotebookAddNoteBlock(type: NoteBlockType.bookmarks),
-                  );
-                  logCreateNoteEvent(NoteBlockType.bookmarks);
-                  Navigator.pop(context);
-                },
-              ),
-            ],
-          ),
-        );
-      },
     );
   }
 }
