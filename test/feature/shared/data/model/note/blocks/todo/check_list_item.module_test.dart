3bef5c6b6815cfcd6b2aae519f375a901c40b0bd
makjac <56642363+makjac@users.noreply.github.com>
2024-09-18 06:24:33 +0200

    feat(test): Add test for converting `ChecklistItemModel` from JSON

--- test/feature/shared/data/model/note/blocks/todo/check_list_item.module_test.dart
+++ test/feature/shared/data/model/note/blocks/todo/check_list_item.module_test.dart
@@ -32,6 +32,20 @@ void main() {
       expect(checklistItemModel.isChecked, checklistItem.isChecked);
     });
 
+    test('should convert JSON to ChecklistItemModel using fromJson', () {
+      final json = {
+        'id': 1,
+        'title': 'Sample Item',
+        'isChecked': true,
+      };
+
+      final checklistItemModel = ChecklistItemModel.fromJson(json);
+
+      expect(checklistItemModel.id, 1);
+      expect(checklistItemModel.title, 'Sample Item');
+      expect(checklistItemModel.isChecked, true);
+    });
+
     test(
         'should compare two ChecklistItemModel instances with the same values as equal',
         () {
