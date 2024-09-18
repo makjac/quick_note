18a63be375043b9cc26f735ae91983a14cee3e8a
makjac <56642363+makjac@users.noreply.github.com>
2024-09-18 06:24:47 +0200

    feat(test): Add test for converting `ChecklistItemModel` to JSON and implementing `toJson` method

--- test/feature/shared/data/model/note/blocks/todo/check_list_item.module_test.dart
+++ test/feature/shared/data/model/note/blocks/todo/check_list_item.module_test.dart
@@ -46,6 +46,20 @@ void main() {
       expect(checklistItemModel.isChecked, true);
     });
 
+    test('should convert ChecklistItemModel to JSON using toJson', () {
+      final checklistItemModel = ChecklistItemModel(
+        id: 1,
+        title: 'Sample Item',
+        isChecked: true,
+      );
+
+      final json = checklistItemModel.toJson();
+
+      expect(json['id'], 1);
+      expect(json['title'], 'Sample Item');
+      expect(json['isChecked'], true);
+    });
+
     test(
         'should compare two ChecklistItemModel instances with the same values as equal',
         () {
