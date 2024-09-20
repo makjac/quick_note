910503314c6eb1cdd04b944ddf986207767c04ff
makjac <56642363+makjac@users.noreply.github.com>
2024-09-21 00:13:55 +0200

    feat: add `NoteExportImportDatasouce` interface

---
+++ lib/feature/import-export/data/datasource/note_export_import_datasouce.dart
@@ -0,0 +1,15 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:quick_note/feature/import-export/data/model/note_data_package.module.dart';
+import 'package:quick_note/feature/shared/data/model/note/note.module.dart';
+
+abstract class NoteExportImportDatasouce {
+  Future<List<NoteModel>> readNotes({
+    required String filePath,
+  });
+  Future<void> writeNotes({
+    required NoteDataPackageModel notesPackage,
+    required String filePath,
+  });
+}
