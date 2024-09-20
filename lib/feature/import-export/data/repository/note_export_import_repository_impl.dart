6132723f76ec4fc0a7616390dbdb1ccd6d7873b7
makjac <56642363+makjac@users.noreply.github.com>
2024-09-21 00:19:43 +0200

    feat: add `NoteExportImportRepositoryImpl` class

---
+++ lib/feature/import-export/data/repository/note_export_import_repository_impl.dart
@@ -0,0 +1,18 @@
+import 'package:dartz/dartz.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:quick_note/core/error/exception/file_not_found_exception.dart';
+import 'package:quick_note/core/error/failure/cache_failure.dart';
+import 'package:quick_note/core/error/failure/failure.dart';
+import 'package:quick_note/core/error/failure/file_not_found_failure.dart';
+import 'package:quick_note/feature/import-export/data/datasource/note_export_import_datasouce.dart';
+import 'package:quick_note/feature/import-export/data/model/note_data_package.module.dart';
+import 'package:quick_note/feature/import-export/domain/repository/note_export_import_repository.dart';
+import 'package:quick_note/feature/shared/domain/entity/note/note.dart';
+
+class NoteExportImportRepositoryImpl implements NoteExportImportRepository {
+  const NoteExportImportRepositoryImpl({
+    required this.noteExportImportDatasouce,
+  });
+
+  final NoteExportImportDatasouce noteExportImportDatasouce;
+}
