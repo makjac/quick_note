3bff63b04c6f6eb08c5f133a055eddeb03315713
makjac <56642363+makjac@users.noreply.github.com>
2024-09-21 15:52:19 +0200

    refactor: update `TodoBlockAddTaskButton` widget with tooltip

--- lib/feature/notebook/presentation/widget/note_block/blocks/todo/todo_block_add_task_button.dart
+++ lib/feature/notebook/presentation/widget/note_block/blocks/todo/todo_block_add_task_button.dart
@@ -7,26 +7,29 @@ class TodoBlockAddTaskButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: Insets.xxs),
-      child: TextButton(
-        onPressed: () => context.read<TodoBlockCubit>().addCheckbox(),
-        style: TextButton.styleFrom(
-          foregroundColor: Theme.of(context).todoAddTaskForegroundButtonColor,
-        ),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: Insets.s,
-            vertical: Insets.xxs,
+      child: Tooltip(
+        message: context.l10n.todo_block_add_task,
+        child: TextButton(
+          onPressed: () => context.read<TodoBlockCubit>().addCheckbox(),
+          style: TextButton.styleFrom(
+            foregroundColor: Theme.of(context).todoAddTaskForegroundButtonColor,
           ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              const Icon(Icons.add),
-              const SizedBox(width: Insets.xs),
-              Text(
-                context.l10n.todo_block_add_task,
-                style: const TextStyle(letterSpacing: .8),
-              ),
-            ],
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: Insets.s,
+              vertical: Insets.xxs,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                const Icon(Icons.add),
+                const SizedBox(width: Insets.xs),
+                Text(
+                  context.l10n.todo_block_add_task,
+                  style: const TextStyle(letterSpacing: .8),
+                ),
+              ],
+            ),
           ),
         ),
       ),
